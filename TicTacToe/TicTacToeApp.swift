1ee732ff6dfe15c236e8b4c94d9a1bd6d0d31598
maciejeicam <maciej_michalik@onet.pl>
2023-03-01 19:01:09 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Maciej Michalik on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
